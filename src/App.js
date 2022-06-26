d39efd63b0c3a8cf486138d41a2c45f11e92bbfe
halilibrahimcelik <halil.celik139@gmail.com>
2022-06-26 19:04:37 +0300

    deploying the project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1>
+        APP
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
+          eveniet.
+        </p>
+        <h2>deneme</h2>
+      </h1>
+    </>
+  );
+}
+
+export default App;
