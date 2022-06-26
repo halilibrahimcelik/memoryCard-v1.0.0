dae1db726d3e94075f12c0d19864dc57dc3bfff1
halilibrahimcelik <halil.celik139@gmail.com>
2022-06-26 19:09:15 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 function App() {
   return (
     <>
-      <h1>
-        APP
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur,
-          eveniet.
-        </p>
-        <h2>deneme</h2>
-      </h1>
+      <h1>APP</h1>
     </>
   );
 }
